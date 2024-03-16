602d7e7fab09fab7f2ea87e51f63e7633a46a1d1
V <truyenhaunhan@gmail.com>
2024-03-16 13:44:13 +0700

    Add 404 html page.

---
+++ public/assets/js/404.js
@@ -0,0 +1,10 @@
+/**
+ * @file 404.js
+ * @description 404 page script file.
+ */
+
+import { global } from './global.js';
+import * as functions from './function.js';
+
+functions.hideHeader();
+functions.hideFooter();
