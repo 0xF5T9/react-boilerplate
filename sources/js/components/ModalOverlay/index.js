728579ece26c4ef1ec1d559b12be68acfedfff1f
V <truyenhaunhan@gmail.com>
2024-05-20 14:15:31 +0700

    Convert modal overlay component css to module, fix styles.

--- sources/js/components/ModalOverlay/index.js
+++ sources/js/components/ModalOverlay/index.js
@@ -1,13 +1,12 @@
 /**
  * @file index.js
  * @description Modal overlay component.
- * @todo TODO: Rework ...
  */
 
 'use strict';
 import { useEffect, useRef } from 'react';
 import SignupForm from '../SignupForm';
-import './ModalOverlay.css';
+import * as styles from './ModalOverlay.module.css';
 const $ = document.querySelector.bind(document);
 
 /**
@@ -18,7 +17,9 @@ const $ = document.querySelector.bind(document);
 function isModalOverlayVisible() {
     const modal_overlay = $('#modal-overlay');
     if (modal_overlay) {
-        return modal_overlay.querySelector('.modal-window.is-open')
+        return modal_overlay.querySelector(
+            `.${styles['modal-window']}.${styles['is-open']}`
+        )
             ? true
             : false;
     }
@@ -27,31 +28,61 @@ function isModalOverlayVisible() {
 
 /**
  * Close the modal overlay.
+ * @param {Bool} skipAnimation Specifies whether to skip animations.
  */
-function closeModalOverlay() {
+function closeModalOverlay(skipAnimation) {
     if (!isModalOverlayVisible()) return;
     const modal_overlay = $('#modal-overlay');
     for (const modal_window of modal_overlay.querySelectorAll(
-        '.modal-window.is-open'
+        `.${styles['modal-window']}.${styles['is-open']}`
     )) {
-        modal_overlay.classList.remove('is-open');
-        modal_overlay.classList.add('is-close');
-        modal_window.classList.remove('is-open');
-        modal_window.classList.add('is-close');
+        modal_overlay.classList.remove(styles['is-open']);
+        modal_overlay.classList.add(styles['is-close']);
+        if (!skipAnimation) {
+            function onOverlayAnimationEnd(event) {
+                if (event.target == modal_overlay)
+                    modal_overlay.classList.add(styles['is-hidden']);
+                modal_overlay.removeEventListener(
+                    'animationend',
+                    onOverlayAnimationEnd
+                );
+            }
+            modal_overlay.addEventListener(
+                'animationend',
+                onOverlayAnimationEnd
+            );
+        } else modal_overlay.classList.add(styles['is-hidden']);
+
+        modal_window.classList.remove(styles['is-open']);
+        modal_window.classList.add(styles['is-close']);
+        if (!skipAnimation) {
+            function onModalWindowAnimationEnd() {
+                modal_window.classList.add(styles['is-hidden']);
+                modal_window.removeEventListener(
+                    'animationend',
+                    onModalWindowAnimationEnd
+                );
+            }
+            modal_window.addEventListener(
+                'animationend',
+                onModalWindowAnimationEnd
+            );
+        } else modal_window.classList.add(styles['is-hidden']);
     }
 }
 
 /**
  * Open a modal window.
- * @param {String} modalWindowId The modal window id.
+ * @param {String} modalWindowId Specifies the modal window id.
  */
-function openModalWindow(modalWindowId = '') {
-    if (isModalOverlayVisible()) closeModalOverlay();
+function openModalWindow(modalWindowId) {
+    if (isModalOverlayVisible()) closeModalOverlay(true);
     const modal_overlay = $('#modal-overlay');
     if (modal_overlay) {
-        if (!modal_overlay.classList.contains('is-open')) {
-            modal_overlay.classList.remove('is-close');
-            modal_overlay.classList.add('is-open');
+        if (!modal_overlay.classList.contains(styles['is-open'])) {
+            modal_overlay.classList.remove(styles['is-hidden']);
+            modal_overlay.classList.remove(styles['is-close']);
+            modal_overlay.classList.add(styles['is-open']);
         }
 
         const modal_window = modal_overlay.querySelector(`#${modalWindowId}`);
@@ -59,8 +90,9 @@ function openModalWindow(modalWindowId = '') {
             console.log(`'#${modalWindowId}' modal window not found.`);
             return;
         }
-        modal_window.classList.add('is-open');
-        modal_window.classList.remove('is-close');
+        modal_window.classList.remove(styles['is-hidden']);
+        modal_window.classList.remove(styles['is-close']);
+        modal_window.classList.add(styles['is-open']);
     }
 }
 
@@ -79,11 +111,13 @@ function ModalOverlay() {
         window.addEventListener('keydown', handleModalOverlayKeypress);
     }, []);
 
+    // Close modal overlay on background click.
     function handleModalOverlayClick(event) {
         if (isModalOverlayVisible() && event.target === modal_overlay.current)
             closeModalOverlay();
     }
 
+    // Close modal overlay on escape key press.
     function handleModalOverlayKeypress(event) {
         if (!isModalOverlayVisible()) return;
         if (event.key === 'Escape') closeModalOverlay();
@@ -91,26 +125,38 @@ function ModalOverlay() {
 
     return (
         <>
-            <div ref={modal_overlay} id="modal-overlay">
+            <div
+                ref={modal_overlay}
+                id="modal-overlay"
+                className={`${styles['modal-overlay']}  ${styles['is-close']} ${styles['is-hidden']}`}
+            >
                 <div
                     id="custom-modal-window-1"
-                    className="modal-window"
+                    className={`${styles['modal-window']}  ${styles['is-close']} ${styles['is-hidden']}`}
+                    style={{ padding: '20px', textAlign: 'center' }}
                     onClick={(event) => event.stopPropagation()}
                 >
                     <h3>Modal Window 1</h3>
-                    <p>Modal window contents.</p>
+                    <p style={{ marginTop: '10px' }}>Modal window contents.</p>
                 </div>
                 <div
                     id="custom-modal-window-2"
-                    className="modal-window"
+                    className={`${styles['modal-window']}  ${styles['is-close']} ${styles['is-hidden']}`}
+                    style={{ padding: '20px', textAlign: 'center' }}
                     onClick={(event) => event.stopPropagation()}
                 >
                     <h3>Modal Window 2</h3>
-                    <p>Modal window contents.</p>
+                    <p style={{ marginTop: '10px' }}>Modal window contents.</p>
                 </div>
                 <div
                     id="signup-form-modal-window"
-                    className="modal-window"
+                    className={`${styles['modal-window']}  ${styles['is-close']} ${styles['is-hidden']}`}
+                    style={{
+                        '--modal-window-width': '310px',
+                        display: 'block',
+                        padding: '20px',
+                        overflow: 'auto',
+                    }}
                     onClick={(event) => event.stopPropagation()}
                 >
                     <SignupForm />
