c72eaeb4b1755a527ef3b600bf3ef51d9fe3ae12
V <truyenhaunhan@gmail.com>
2024-05-21 22:45:02 +0700

    Add json-server database endpoints config.

--- sources/configs/index.js
+++ sources/configs/index.js
@@ -5,9 +5,11 @@
 
 'use strict';
 import routes from './routes.js';
+import jsdbEndpoints from './jsdb-endpoints.js';
 
 const configs = {
     routes,
+    jsdbEndpoints,
 };
 
 export default configs;
---
+++ sources/configs/jsdb-endpoints.js
@@ -0,0 +1,14 @@
+/**
+ * @file jsdb-endpoints.js
+ * @description json-server database endpoints.
+ */
+
+'use strict';
+
+const jsdbEndpoints = {
+    posts: 'posts',
+    comments: 'comments',
+    profile: 'profile',
+};
+
+export default jsdbEndpoints;
