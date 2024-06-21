84216bab7e0d2efe427af8f954b98099f658ed1b
V <truyenhaunhan@gmail.com>
2024-06-21 23:12:38 +0700

    Update code formats, comments, ..

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 /**
  * @file app.js
- * @description Start the web-server using Express.
+ * @description Start the application using Express.
  */
 
-const express = require('express'),
-    // fs = require('fs'),
-    path = require('path'),
-    app = express(),
-    port = 8317;
+'use strict';
 
+// Built-in module(s).
+const path = require('path');
+
+// External module(s).
+const express = require('express');
+
+// Express configurations.
+const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Path configuration to respond to a request to static route request by serving 'index.html'
-// React router won't work without this.
-// https://stackoverflow.com/questions/44491184/react-router-does-not-work-in-production-and-surge-deployments
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Routes:
+app.get('/*', (request, result) => {
+    // Path configuration to respond to a request to static route request by serving 'index.html'
+    // React router won't work without this.
+    // https://stackoverflow.com/questions/44491184/react-router-does-not-work-in-production-and-surge-deployments
+    result.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use(function (error, req, res, next) {
-    res.status(500).send('500: Internal Server Error');
+// Error-handling middleware.
+app.use((error, request, result, next) => {
+    console.error(
+        'THIS ERROR IS CAPTURED BY SPECIAL ERROR-HANDLING MIDDLEWARE.'
+    );
+    const status_code = error.statusCode || 500;
+    console.error(error.message, error.stack);
+    result.status(status_code).json({ message: error.message });
 });
 
+// Launch server.
+const port = 8317;
 app.listen(port, function () {
-    console.log(`Running at port ${port}.`);
+    console.log(`Application is listening on port ${port}.`);
 });
