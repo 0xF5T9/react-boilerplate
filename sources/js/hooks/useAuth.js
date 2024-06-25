1d9178d77b1dfbe2726cb23cbe8d3c8be7605903
V <truyenhaunhan@gmail.com>
2024-06-25 14:19:59 +0700

    Add useAuth & useLocalStorage hooks for authentication implementation.

---
+++ sources/js/hooks/useAuth.js
@@ -0,0 +1,66 @@
+/**
+ * @file useAuth.js
+ * @description Authentication hook.
+ */
+
+'use strict';
+import { useContext, useMemo, createContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLocalStorage } from './useLocalStorage';
+import PropTypes from 'prop-types';
+
+// Authentication context.
+const AuthContext = createContext();
+
+/**
+ * Authentication context provider component.
+ * @param {Object} props Component properties.
+ * @param {*} props.children Context children.
+ * @returns Returns the component.
+ */
+function AuthProvider({ children }) {
+    const [authSession, setAuthSession] = useLocalStorage('authSession', null),
+        navigate = useNavigate();
+
+    async function login(sessionData) {
+        const { username, token } = sessionData;
+        if (!username || !token) {
+            console.error('Invalid session data.');
+            navigate('/');
+        }
+
+        setAuthSession(sessionData);
+        navigate('/secret'); // Redirect to secret route on successful login. [MOCK]
+    }
+
+    async function logout() {
+        setAuthSession(null);
+        navigate('/', { replace: true });
+    }
+
+    const value = useMemo(
+        () => ({
+            authSession,
+            login,
+            logout,
+        }),
+        [authSession]
+    );
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node,
+};
+
+/**
+ * Hook provides a convenient way to access and manage user-
+ * authentication status from application components.
+ */
+function useAuth() {
+    return useContext(AuthContext);
+}
+
+export { useAuth, AuthProvider };
