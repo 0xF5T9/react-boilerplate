acd16bb65914e7931fe7a342ae9db404594e99cc
V <truyenhaunhan@gmail.com>
2024-06-30 22:26:27 +0700

    Add loader function(s).

--- sources/js/components/Sections/index.js
+++ sources/js/components/Sections/index.js
@@ -1,7 +1,10 @@
 export { default as IndexSection } from './IndexSection';
 export { default as LoginSection } from './LoginSection';
 export { default as RegisterSection } from './RegisterSection';
-export { default as ProfileSection } from './ProfileSection';
+export {
+    default as ProfileSection,
+    loader as ProfileSectionLoader,
+} from './ProfileSection';
 export { default as ButtonSampleSection } from './ButtonSampleSection';
 export { default as InputSampleSection } from './InputSampleSection';
 export { default as CheckboxSampleSection } from './CheckboxSampleSection';
