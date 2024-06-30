a6339e9036f23b0a0ed762fe8819ff79c3a11d91
V <truyenhaunhan@gmail.com>
2024-06-30 22:20:46 +0700

    Add 'invalidToken' property, to be used by protected api.

--- sources/js/utility/api.js
+++ sources/js/utility/api.js
@@ -14,12 +14,19 @@ class APIResponse {
      * @param {String} [message=''] Response message.
      * @param {Boolean} [success=false] Specifies whether the action is successful.
      * @param {Object=} [data=null] Response associated data.
+     * @param {Boolean} [invalidToken=false]  Specifies whether the action is rejected due to an invalid token.
      * @returns {Object} Returns the response object.
      */
-    constructor(message = '', success = false, data = null) {
+    constructor(
+        message = '',
+        success = false,
+        data = null,
+        invalidToken = false
+    ) {
         this.message = message;
         this.success = success;
         this.data = data;
+        this.invalidToken = invalidToken;
     }
 }
 
