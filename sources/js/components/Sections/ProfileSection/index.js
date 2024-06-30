0bed04adf1cb8f302b9ad57be05020843c94cbc3
V <truyenhaunhan@gmail.com>
2024-06-30 23:34:32 +0700

    Add test code.

--- sources/js/components/Sections/ProfileSection/index.js
+++ sources/js/components/Sections/ProfileSection/index.js
@@ -5,32 +5,100 @@
  */
 
 'use strict';
-import { useState } from 'react';
-import { useAuth } from '../../../hooks/useAuth';
+import { useState, useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { useAuth, getAuthSession } from '../../../hooks/useAuth';
+
+import apis from '../../../apis';
+import { routes } from '../../../configs/react-router';
 
 import ContentSection from '../../Content/components/ContentSection';
 import Button from '../../Button';
+import { showToast } from '../../ToastOverlay';
+
+/**
+ * Loader component.
+ * @returns {*} Returns the loader data.
+ */
+async function loader() {
+    const authSession = getAuthSession();
+    if (!authSession)
+        // NOTE:
+        // Polyfill the loader object as the component only expects an APIResponse object.
+        // If the 'authSession' is null and <ProtectedRoute /> component didn't prevent-
+        // this component from being rendered, meaning the 'authSession' data has been tampered.
+        return {
+            message:
+                'Invalid session detected. This incident will be reported.',
+            success: false,
+            data: null,
+            invalidToken: true,
+        };
+
+    const { username, token } = authSession;
+
+    return await apis.mysqlServer.getUserInfo(username, token);
+}
 
 /**
  * Profile section.
  * @returns Returns the component.
  */
 function ProfileSection() {
-    const { authSession, logout } = useAuth();
+    const loaderData = useLoaderData(),
+        { authSession, logout } = useAuth();
 
-    async function handleLogout(event) {
-        logout();
-    }
+    const [userInfo, setUserInfo] = useState();
+
+    useEffect(() => {
+        (async () => {
+            const { message, success, data, invalidToken } = loaderData;
+            if (!success) {
+                setTimeout(
+                    () => showToast('Error', message, 'error', 5000),
+                    100
+                );
+                logout(invalidToken ? routes.login : routes.home);
+            }
+            setUserInfo(data);
+        })();
+    }, [loaderData]);
 
     return (
         <>
             <ContentSection flexCenter textCenter>
-                <h1>Profile Section</h1>
-                <h3>Logged as {authSession.username}</h3>
-                <Button onClick={handleLogout}>Logout</Button>
+                {userInfo && (
+                    <>
+                        <h1>Profile Section</h1>
+                        <h3>
+                            Logged as {authSession.username} ({userInfo.email})
+                        </h3>
+                        <h3>
+                            Created At:{' '}
+                            {new Date(userInfo.createdAt).toLocaleString(
+                                'en-US'
+                            )}
+                        </h3>
+                        <Button onClick={() => logout()}>Logout</Button>
+                        <Button
+                            onClick={() => {
+                                (async () => {
+                                    console.log(
+                                        await apis.mysqlServer.verifySession(
+                                            authSession
+                                        )
+                                    );
+                                })();
+                            }}
+                        >
+                            Verify
+                        </Button>
+                    </>
+                )}
             </ContentSection>
         </>
     );
 }
 
 export default ProfileSection;
+export { loader };
