b79279a5bee8094c34405a59e8b392098aafcdac
V <truyenhaunhan@gmail.com>
2024-07-14 14:37:06 +0700

    Add: theme components.

---
+++ sources/js/components/Theme/index.js
@@ -0,0 +1,16 @@
+/**
+ * @file index.js
+ * @description Theme components.
+ */
+'use strict';
+import MonokaiPro from './MonokaiPro';
+import MonokaiSpectrum from './MonokaiSpectrum';
+
+const Themes = {
+    MonokaiPro,
+    MonokaiSpectrum,
+};
+
+export default Themes;
+export { default as MonokaiPro } from './MonokaiPro';
+export { default as MonokaiSpectrum } from './MonokaiSpectrum';
