87f54ef6e79186a449a759922f02cfc7c45c1dc9
V <truyenhaunhan@gmail.com>
2024-08-03 13:40:16 +0700

    Update button sample section.

--- sources/js/components/Sections/ButtonSampleSection/index.tsx
+++ sources/js/components/Sections/ButtonSampleSection/index.tsx
@@ -39,52 +39,110 @@ function ButtonSampleSection() {
                 <Button color="blue">Blue</Button>{' '}
                 <Button color="purple">Purple</Button>
                 <h1 style={{ marginTop: '26px' }}>Default White Buttons</h1>
-                <Button white>Primary</Button>{' '}
-                <Button color="red" white>
-                    Red
-                </Button>{' '}
-                <Button color="orange" white>
-                    Orange
-                </Button>{' '}
-                <Button color="yellow" white>
-                    Yellow
-                </Button>{' '}
-                <Button color="green" white>
-                    Green
-                </Button>{' '}
-                <Button color="blue" white>
-                    Blue
-                </Button>{' '}
-                <Button color="purple" white>
-                    Purple
-                </Button>
+                <div
+                    style={{
+                        background: 'var(--background-dark)',
+                        padding: '5px 10px 10px 10px',
+                        borderRadius: '10px',
+                    }}
+                >
+                    <span
+                        style={{
+                            display: 'block',
+                            margin: '4px 0px',
+                            color: 'var(--color-green',
+                        }}
+                    >
+                        <i className="fas fa-circle-info"></i> For dark
+                        background only
+                    </span>
+                    <Button white>Primary</Button>{' '}
+                    <Button color="red" white>
+                        Red
+                    </Button>{' '}
+                    <Button color="orange" white>
+                        Orange
+                    </Button>{' '}
+                    <Button color="yellow" white>
+                        Yellow
+                    </Button>{' '}
+                    <Button color="green" white>
+                        Green
+                    </Button>{' '}
+                    <Button color="blue" white>
+                        Blue
+                    </Button>{' '}
+                    <Button color="purple" white>
+                        Purple
+                    </Button>
+                </div>
                 <h1 style={{ marginTop: '26px' }}>Outline White Buttons</h1>
-                <Button outline white>
-                    Primary
-                </Button>{' '}
-                <Button color="red" outline white>
-                    Red
-                </Button>{' '}
-                <Button color="orange" outline white>
-                    Orange
-                </Button>{' '}
-                <Button color="yellow" outline white>
-                    Yellow
-                </Button>{' '}
-                <Button color="green" outline white>
-                    Green
-                </Button>{' '}
-                <Button color="blue" outline white>
-                    Blue
-                </Button>{' '}
-                <Button color="purple" outline white>
-                    Purple
-                </Button>
+                <div
+                    style={{
+                        background: 'var(--background-dark)',
+                        padding: '5px 10px 10px 10px',
+                        borderRadius: '10px',
+                    }}
+                >
+                    <span
+                        style={{
+                            display: 'block',
+                            margin: '4px 0px',
+                            color: 'var(--color-green',
+                        }}
+                    >
+                        <i className="fas fa-circle-info"></i> For dark
+                        background only
+                    </span>
+                    <Button outline white>
+                        Primary
+                    </Button>{' '}
+                    <Button color="red" outline white>
+                        Red
+                    </Button>{' '}
+                    <Button color="orange" outline white>
+                        Orange
+                    </Button>{' '}
+                    <Button color="yellow" outline white>
+                        Yellow
+                    </Button>{' '}
+                    <Button color="green" outline white>
+                        Green
+                    </Button>{' '}
+                    <Button color="blue" outline white>
+                        Blue
+                    </Button>{' '}
+                    <Button color="purple" outline white>
+                        Purple
+                    </Button>
+                </div>
                 <h1 style={{ marginTop: '26px' }}>White-Only Buttons</h1>
-                <Button whiteOnly>Default</Button>{' '}
-                <Button outline whiteOnly>
-                    Outline
-                </Button>
+                <div
+                    style={{
+                        background: 'var(--background-dark)',
+                        padding: '5px 10px 10px 10px',
+                        borderRadius: '10px',
+                        width: 'fit-content',
+                        justifySelf: 'center',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                    }}
+                >
+                    <span
+                        style={{
+                            display: 'block',
+                            margin: '4px 0px',
+                            color: 'var(--color-green',
+                        }}
+                    >
+                        <i className="fas fa-circle-info"></i> For dark
+                        background only
+                    </span>
+                    <Button whiteOnly>Default</Button>{' '}
+                    <Button outline whiteOnly>
+                        Outline
+                    </Button>
+                </div>
                 <h1 style={{ marginTop: '26px' }}>Disabled Buttons</h1>
                 <Button disabled>Primary</Button>{' '}
                 <Button disabled color="red">
@@ -106,78 +164,97 @@ function ButtonSampleSection() {
                     Purple
                 </Button>
                 <br />
-                <Button disabled white>
-                    Primary
-                </Button>{' '}
-                <Button disabled color="red" white>
-                    Red
-                </Button>{' '}
-                <Button disabled color="orange" white>
-                    Orange
-                </Button>{' '}
-                <Button disabled color="yellow" white>
-                    Yellow
-                </Button>{' '}
-                <Button disabled color="green" white>
-                    Green
-                </Button>{' '}
-                <Button disabled color="blue" white>
-                    Blue
-                </Button>{' '}
-                <Button disabled color="purple" white>
-                    Purple
-                </Button>
-                <br />
-                <Button disabled outline>
-                    Primary
-                </Button>{' '}
-                <Button disabled color="red" outline>
-                    Red
-                </Button>{' '}
-                <Button disabled color="orange" outline>
-                    Orange
-                </Button>{' '}
-                <Button disabled color="yellow" outline>
-                    Yellow
-                </Button>{' '}
-                <Button disabled color="green" outline>
-                    Green
-                </Button>{' '}
-                <Button disabled color="blue" outline>
-                    Blue
-                </Button>{' '}
-                <Button disabled color="purple" outline>
-                    Purple
-                </Button>
-                <br />
-                <Button disabled outline white>
-                    Primary
-                </Button>{' '}
-                <Button disabled color="red" outline white>
-                    Red
-                </Button>{' '}
-                <Button disabled color="orange" outline white>
-                    Orange
-                </Button>{' '}
-                <Button disabled color="yellow" outline white>
-                    Yellow
-                </Button>{' '}
-                <Button disabled color="green" outline white>
-                    Green
-                </Button>{' '}
-                <Button disabled color="blue" outline white>
-                    Blue
-                </Button>{' '}
-                <Button disabled color="purple" outline white>
-                    Purple
-                </Button>
-                <br />
-                <Button disabled whiteOnly>
-                    Default
-                </Button>{' '}
-                <Button disabled outline whiteOnly>
-                    Outline
-                </Button>
+                <div
+                    style={{
+                        background: 'var(--background-dark)',
+                        padding: '5px 10px 10px 10px',
+                        marginTop: '10px',
+                        borderRadius: '10px',
+                    }}
+                >
+                    <span
+                        style={{
+                            display: 'block',
+                            margin: '4px 0px',
+                            color: 'var(--color-green',
+                        }}
+                    >
+                        <i className="fas fa-circle-info"></i> For dark
+                        background only
+                    </span>
+                    <Button disabled white>
+                        Primary
+                    </Button>{' '}
+                    <Button disabled color="red" white>
+                        Red
+                    </Button>{' '}
+                    <Button disabled color="orange" white>
+                        Orange
+                    </Button>{' '}
+                    <Button disabled color="yellow" white>
+                        Yellow
+                    </Button>{' '}
+                    <Button disabled color="green" white>
+                        Green
+                    </Button>{' '}
+                    <Button disabled color="blue" white>
+                        Blue
+                    </Button>{' '}
+                    <Button disabled color="purple" white>
+                        Purple
+                    </Button>
+                    <br />
+                    <Button disabled outline>
+                        Primary
+                    </Button>{' '}
+                    <Button disabled color="red" outline>
+                        Red
+                    </Button>{' '}
+                    <Button disabled color="orange" outline>
+                        Orange
+                    </Button>{' '}
+                    <Button disabled color="yellow" outline>
+                        Yellow
+                    </Button>{' '}
+                    <Button disabled color="green" outline>
+                        Green
+                    </Button>{' '}
+                    <Button disabled color="blue" outline>
+                        Blue
+                    </Button>{' '}
+                    <Button disabled color="purple" outline>
+                        Purple
+                    </Button>
+                    <br />
+                    <Button disabled outline white>
+                        Primary
+                    </Button>{' '}
+                    <Button disabled color="red" outline white>
+                        Red
+                    </Button>{' '}
+                    <Button disabled color="orange" outline white>
+                        Orange
+                    </Button>{' '}
+                    <Button disabled color="yellow" outline white>
+                        Yellow
+                    </Button>{' '}
+                    <Button disabled color="green" outline white>
+                        Green
+                    </Button>{' '}
+                    <Button disabled color="blue" outline white>
+                        Blue
+                    </Button>{' '}
+                    <Button disabled color="purple" outline white>
+                        Purple
+                    </Button>
+                    <br />
+                    <Button disabled whiteOnly>
+                        Default
+                    </Button>{' '}
+                    <Button disabled outline whiteOnly>
+                        Outline
+                    </Button>
+                </div>
                 <h1 style={{ marginTop: '26px' }}>Icon Buttons</h1>
                 <Button color="green">
                     <i className="fa-solid fa-plus"></i> Create
