3c474601a366a890d5a67b7530d14510b601f29f
V <truyenhaunhan@gmail.com>
2024-08-26 20:57:45 +0700

    Update grid component

--- sources/js/components/ProtectedRoute/index.tsx
+++ sources/js/components/ProtectedRoute/index.tsx
@@ -62,13 +62,15 @@ const ProtectedRoute: FunctionComponent<{ children?: ReactNode }> = function ({
     if (isVerifying)
         return (
             <FlexibleSection
-                style={{
-                    display: 'flex',
-                    flexFlow: 'column nowrap',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    padding: '50px 20px',
-                    textAlign: 'center',
+                contentProps={{
+                    style: {
+                        display: 'flex',
+                        flexFlow: 'column nowrap',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '50px 20px',
+                        textAlign: 'center',
+                    },
                 }}
             >
                 <CircleLoading style={{ width: '30px' }} />
